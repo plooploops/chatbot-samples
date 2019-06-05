c9335336ddc07d885bdbf33bfea44a4ba1ebdd20
Andy <andy.gee@microsoft.com>
2019-06-04 20:34:00 -0700

    wip adaptive card submit

--- EchoBot1/EchoBot1/Dialogs/AdaptiveCardDialog.cs
+++ EchoBot1/EchoBot1/Dialogs/AdaptiveCardDialog.cs
@@ -10,35 +10,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using AdaptiveCards;
+using EchoBot1.Prompts;
 
 namespace EchoBot1.Dialogs
 {
     public class AdaptiveCardDialog : ComponentDialog
     {
+        private string selectedDay { get; set; }
         public AdaptiveCardDialog(string dialogId) : base(dialogId)
         {
             // ID of the child dialog that should be started anytime the component is started.
             this.InitialDialogId = dialogId;
-
+            this.AddDialog(new CustomPromptWrapper("customPromptWrapper"));
             // Define the conversation flow using the waterfall model.
             this.AddDialog(
                 new WaterfallDialog(dialogId, new WaterfallStep[]
                     {
                         async (stepContext, ct) =>
-                        {
+                        {                            
                             await stepContext.Context.SendActivityAsync("[Adaptive Card Dialog] - Adaptive Card Test!");
 
                             Attachment attachment = new Attachment()
                             {
-
                                 ContentType = AdaptiveCard.ContentType,
-                                Content = await AdaptiveCardService.GetAdaptiveCardByFileName(@".\Cards\sample-table.json")
+                                Content = await AdaptiveCardService.GetAdaptiveCardByFileName(@".\Cards\multiple-input-submit.json")
                             };
 
                             var reply = stepContext.Context.Activity.CreateReply();
                             reply.Attachments.Add(attachment);
 
-                            await stepContext.Context.SendActivityAsync(reply);
+                            return await stepContext.PromptAsync(
+                                "customPromptWrapper",
+                                new PromptOptions
+                                {
+                                    Prompt = reply
+                                },
+                                ct
+                            ).ConfigureAwait(false);
+                        },
+                         async (stepContext, ct) =>
+                        {
+                            //https://stackoverflow.com/questions/53009106/adaptive-card-response-from-a-waterfallstep-dialog-ms-bot-framework-v4
+                            var userAnswer = (string) stepContext.Result;
 
                             return await stepContext.NextAsync().ConfigureAwait(false);
                         }
---
+++ EchoBot1/EchoBot1/Prompts/CustomPromptWrapper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace EchoBot1.Prompts
+{
+    //https://stackoverflow.com/questions/53009106/adaptive-card-response-from-a-waterfallstep-dialog-ms-bot-framework-v4
+    public class CustomPromptWrapper : PromptWrapper<string>
+    {
+        public CustomPromptWrapper(string dialogId, PromptValidator<string> validator = null)
+            : base(dialogId, validator)
+        {
+        }
+
+        protected async override Task OnPromptAsync(ITurnContext turnContext, IDictionary<string, object> state, PromptOptions options, bool isRetry, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext == null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (isRetry && options.RetryPrompt != null)
+            {
+                await turnContext.SendActivityAsync(options.RetryPrompt, cancellationToken).ConfigureAwait(false);
+            }
+            else if (options.Prompt != null)
+            {
+                await turnContext.SendActivityAsync(options.Prompt, cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        protected override Task<PromptRecognizerResult<string>> OnRecognizeAsync(ITurnContext turnContext, IDictionary<string, object> state, PromptOptions options, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext == null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
+            var result = new PromptRecognizerResult<string>();
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var message = turnContext.Activity.AsMessageActivity();
+                if (!string.IsNullOrEmpty(message.Text))
+                {
+                    result.Succeeded = true;
+                    result.Value = message.Text;
+                }
+                /*Add handling for Value from adaptive card*/
+                else if (message.Value != null)
+                {
+                    result.Succeeded = true;
+                    result.Value = message.Value.ToString();
+                }
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+
+}
