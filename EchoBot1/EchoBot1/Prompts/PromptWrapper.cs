50c5c70cb081c66aed9f081ed8fb71dd209a5dfa
Andy <andy.gee@microsoft.com>
2019-06-11 15:14:32 -0700

    wip adaptive card submit

--- EchoBot1/EchoBot1/Prompts/PromptWrapper.cs
+++ EchoBot1/EchoBot1/Prompts/PromptWrapper.cs
@@ -8,7 +8,6 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Bot.Schema;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace EchoBot1.Prompts
 {
