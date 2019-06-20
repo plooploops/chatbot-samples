06966ed4bec44717c92b14f5b9daca77d380a550
Andy <andy.gee@microsoft.com>
2019-06-20 10:22:50 -0700

    wip html test

---
+++ EchoBot1/EchoBot1/Dialogs/HTMLtoJSONDialog.cs
@@ -0,0 +1,42 @@
+﻿using EchoBot1.Services;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using AdaptiveCards;
+using EchoBot1.Prompts;
+using EchoBot1.Helper;
+
+namespace EchoBot1.Dialogs
+{
+    public class HTMLToJSONDialog : ComponentDialog
+    {
+
+        public HTMLToJSONDialog(string dialogId) : base(dialogId)
+        {
+            // ID of the child dialog that should be started anytime the component is started.
+            this.InitialDialogId = dialogId;
+            // Define the conversation flow using the waterfall model.
+            this.AddDialog(
+                new WaterfallDialog(dialogId, new WaterfallStep[]
+                    {
+                        async (stepContext, ct) =>
+                        {
+                            string json = HTMLHelper.TestHTML();
+
+                            await stepContext.Context.SendActivityAsync(json);
+
+                            return await stepContext.NextAsync().ConfigureAwait(false);
+                        }
+                    }
+                )
+            );
+        }
+    }
+}
